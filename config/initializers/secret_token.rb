7cdc7dcda320c5a01eb42dd906b06196a16a9ff5
Sean Iams <sean.iams@gmail.com>
2011-08-13 23:56:47 -0500

    Importing my project to Git, without saving history.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobonotes::Application.config.secret_token = 'f8ec1b8963626beb05c2eacef392812a4e65de4cf0cfc3165dc0b9b191719ad464dc129296b37929ae91dc21c979c20e6cfdc81501a1e6fe49035f2ea0bb72f1'
