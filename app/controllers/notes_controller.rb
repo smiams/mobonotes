6ee9dba2ad24c38c3498f8edcbc06f562bbfd37c
Sean Iams <sean.iams@gmail.com>
2011-08-14 13:32:35 -0500

    Notes can be created through a web-form.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,12 @@
 class NotesController < ApplicationController
+  def new
+    @note = Note.new
+  end
+  
+  def create
+    @note = Note.new(params[:note])
+    @note.save
+    
+    head :created, :location => @note.id
+  end
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe NotesController do
-  it "should be true" do
-    true.should == true
+  describe "GET new" do
+    it "renders the new template" do
+      get :new
+      response.should render_template("new")
+    end
+  end
+  
+  describe "POST create" do
+    it "creates a new Note" do
+      post :create, :content => "This is a new note."
+      response.code.should == "201"
+    end
   end
 end
