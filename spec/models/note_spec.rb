409f16113f8c48ad5b6143ae6a0b1d8e3d3780ef
Sean Iams <sean.iams@gmail.com>
2011-08-14 12:52:53 -0500

    added validations to the Note model.

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe Note do
-  it "should be true" do
-    true.should == true
+  context "new notes" do
+    before :each do
+      @note = Note.new
+    end
+    
+    context "without content" do
+      it "should be invalid" do
+        @note.valid?.should == false
+        @note.errors.on(:content).should == "can't be blank"
+      end
+    end
+    
+    context "with content" do
+      before :each do
+        @note.content = "This is a new note."
+      end
+
+      it "should be valid" do
+        @note.valid?.should == true
+      end
+  
+      it "should be creatd" do
+        expect {
+          @note.save.should == true
+        }.to change {Note.count}.from(0).to(1)
+      end
+    end
   end
 end
