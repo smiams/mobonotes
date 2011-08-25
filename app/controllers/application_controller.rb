62dcebb1e578a60db1b11a3c22f31f03f16e3359
Sean Iams <sean.iams@gmail.com>
2011-08-25 00:19:34 -0500

    not requiring login yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :get_current_user
+  # before_filter :get_current_user
   before_filter :_set_time_zone
   
   def get_current_user
-    @current_user = User.exists?(session[:user_id]) ? User.find(session[:user_id]) : nil    
+    @current_user = session[:user_id] && User.exists?(session[:user_id]) ? User.find(session[:user_id]) : nil
     redirect_to login_path if @current_user.nil?
   end
 
