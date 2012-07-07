19d981d7be8638f2729380a7b2c2561d6226e179
Sean Iams <siams@groupon.com>
2012-07-07 15:06:21 -0700

    forgot to limit the tasks to tasks existing under the current label;

--- app/controllers/labels/dates_controller.rb
+++ app/controllers/labels/dates_controller.rb
@@ -3,8 +3,8 @@ class Labels::DatesController < ApplicationController
 
   def show
     @notes = @label.notes.created_between(@date.beginning_of_day.utc, @date.end_of_day.utc)
-    @tasks = @current_user.tasks.occurs_between(@date.beginning_of_day.utc, @date.end_of_day.utc)
-    @tasks += @current_user.tasks.occurs_before(@date.end_of_day.utc).rolling.incomplete
+    @tasks = @label.tasks.occurs_between(@date.beginning_of_day.utc, @date.end_of_day.utc)
+    @tasks += @label.tasks.occurs_before(@date.end_of_day.utc).rolling.incomplete
 
     respond_to do |format|
       format.html { render "dates/show" }
