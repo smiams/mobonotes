dd5f391095f367787f959827a0baec2506be24ec
Sean Iams <siams@groupon.com>
2012-07-07 15:07:37 -0700

    uniq tasks;

--- app/controllers/labels/dates_controller.rb
+++ app/controllers/labels/dates_controller.rb
@@ -5,6 +5,7 @@ class Labels::DatesController < ApplicationController
     @notes = @label.notes.created_between(@date.beginning_of_day.utc, @date.end_of_day.utc)
     @tasks = @label.tasks.occurs_between(@date.beginning_of_day.utc, @date.end_of_day.utc)
     @tasks += @label.tasks.occurs_before(@date.end_of_day.utc).rolling.incomplete
+    @tasks.uniq!
 
     respond_to do |format|
       format.html { render "dates/show" }
