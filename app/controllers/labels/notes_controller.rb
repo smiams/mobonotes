f5636ad42da5fd61fc6c974cddb27a1c97aef952
Sean Iams <sean.iams@gmail.com>
2014-08-22 19:56:57 -0700

    wip: styled the notes/index page;

--- app/controllers/labels/notes_controller.rb
+++ app/controllers/labels/notes_controller.rb
@@ -1,8 +1,8 @@
 class Labels::NotesController < ApplicationController
   def index
     @label = Label.find(params[:label_id])
-    @label_with_tasks = Label.where(:id => @label.id).with_current_tasks_for_user(@current_user, @start_time, @end_time).first
+    @notes = @label.notes.created_between(@start_time, @end_time).where(:user => @current_user).eager_load(:task).order(:created_at)
 
-    render :template => "labels/show"
+    render :template => "notes/index"
   end
 end
